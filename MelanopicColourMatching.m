52f5d681588b36d4bda8bc4bf87f69ef9f3068c1
Tsvetomira Dumbalska <tsvetomira.dumbalska@gmail.com>
2018-07-14 16:56:42 +0100

    added plots and started working on creating CMFs for different sets of primaries

--- MelanopicColourMatching.m
+++ MelanopicColourMatching.m
@@ -47,11 +47,12 @@ M = T_receptors(2,:);
 S = T_receptors(3,:);
 mel = T_receptors(4,:);
 
+
 % COLOR MATCHING DATA
 % -------------------
 mat = csvread('sbrgb10w.csv');
 wls_snb = mat(:, 1);
-T_rgb = mat(:, 2:4);
+snb_rgb = mat(:, 2:4);
 
 % Construct match and test spectra
 r_snb = round(645.16);
@@ -61,40 +62,142 @@ b_snb = round(444.44);
 n_snb = length(wls_snb);
 n_wls = length(wls);
 wls2 = repmat(wls,1,n_snb);
-spd1 = zeros(n_wls,n_snb);
-spd2 = zeros(n_wls,n_snb);
+spdt = zeros(n_wls,n_snb);
+spdm = zeros(n_wls,n_snb);
 
-sigma = 2; % stdev of gaussian
+% constructing spectra assuming a gaussian shape of primaries and test
+FWHM = 10; % FWMH of gaussian function for spectra; based on interference filter properties
+sigma = FWHM/2.4; % stdev of gaussian
 gauss = @(height,position)height.*exp(-((wls-position).^2)/(2*sigma^2));
 
 for i=1:n_snb
-    % using delta function for primaries and test lights
-    % spd1(wls2(:,i)==wls_snb(i),i)=1;  % test light
-    % spd2(wls2(:,i)==r_snb,i)=T_rgb(i,1);  % red priamry
-    % spd2(wls2(:,i)==g_snb,i)=T_rgb(i,2);  % green primary
-    % spd2(wls2(:,i)==b_snb,i)=T_rgb(i,3);  % blue primary
-    
-    % using gaussian function with FMWH ~ 2.4*sigma
-    spd1(:,i) = spd1(:,i)+gauss(1,wls_snb(i));
-    spd2(:,i) = spd2(:,i)+gauss(T_rgb(i,1),r_snb)+gauss(T_rgb(i,2),g_snb)+gauss(T_rgb(i,3),b_snb);
+    spdt(:,i) = spdt(:,i)+gauss(1,wls_snb(i));
+    spdm(:,i) = spdm(:,i)+gauss(snb_rgb(i,1),r_snb)+gauss(snb_rgb(i,2),g_snb)+gauss(snb_rgb(i,3),b_snb);
 end
 
+% % if using delta function to construct spectra uncomment below:
+% for i=1:n_snb
+%     spd1(wls2(:,i)==wls_snb(i),i)=1;  % test light
+%     spd2(wls2(:,i)==r_snb,i)=snb_rgb(i,1);  % red priamry
+%     spd2(wls2(:,i)==g_snb,i)=snb_rgb(i,2);  % green primary
+%     spd2(wls2(:,i)==b_snb,i)=snb_rgb(i,3);  % blue primary
+% end
+
 % plot test and match spectra
-figure; plot(wls,spd1); title('test spectra');
-figure; plot(wls,spd2); title('match spectra');
+figure; hold on; 
+subplot(2,1,1); plot(wls,spdt); title('test spectra'); pbaspect([3 1 1]);
+subplot(2,1,2); plot(wls,spdm); title('match spectra'); pbaspect([3 1 1]);
+% plot one example test and match spectrum
+figure; hold on; 
+subplot(2,1,1); hold on; plot(wls,spdt(:,20)); title('test spectrum'); plot(xlim, [0,0], 'k:'); pbaspect([3 1 1]);
+subplot(2,1,2); hold on; plot(wls,spdm(:,20)); title('match spectrum'); plot(xlim, [0,0], 'k:'); pbaspect([3 1 1]);
 
 
-% Look at cone and melanopsin contrast
-mel_cont = mel*(spd1-spd2)./(mel*(spd2));
-L_cont = L*(spd1-spd2)./(L*(spd2));
-M_cont = M*(spd1-spd2)./(M*(spd2));
-S_cont = S*(spd1-spd2)./(S*(spd2));
+% MELANOPSIN AND CONE STIMULATION
+% -------------------
 
-% Plot as a function of contrast
-figure; hold on;
+% calculate cone and melanopsin stimulation
+L_stimt = L*spdt;
+L_stimm = L*spdm;
+M_stimt = M*spdt;
+M_stimm = M*spdm;
+S_stimt = S*spdt;
+S_stimm = S*spdm;
+mel_stimt = mel*spdt;
+mel_stimm = mel*spdm;
+% plot cone and melanopic stimulation as a function of wavelength
+figure; hold on; 
+plot(wls_snb,L_stimt,'r'); plot(wls_snb,L_stimm,'rx');
+plot(wls_snb,M_stimt,'g'); plot(wls_snb,M_stimm,'gx');  
+plot(wls_snb,S_stimt,'b'); plot(wls_snb,S_stimm,'bx'); 
+plot(wls_snb,mel_stimt,'c'); plot(wls_snb,mel_stimm,'cx'); 
+legend({'L cone test', 'L cone macth', 'M cone test', 'M cone match', 'S cone test', 'S cone match', 'melanopsin test', 'melanopsin match'})
+% zooming in on individuals receptor types
+figure; hold on; 
+subplot(2,2,1); hold on; plot(wls_snb,L_stimt,'r'); plot(wls_snb,L_stimm,'rx'); title('L cone stimulation'); plot(xlim, [0,0], 'k:'); pbaspect([2 1 1]);
+subplot(2,2,2); hold on; plot(wls_snb,M_stimt,'g'); plot(wls_snb,M_stimm,'gx'); title('M cone stimulation'); plot(xlim, [0,0], 'k:'); pbaspect([2 1 1]);
+subplot(2,2,3); hold on; plot(wls_snb,S_stimt,'b'); plot(wls_snb,S_stimm,'bx'); title('S cone stimulation'); plot(xlim, [0,0], 'k:'); pbaspect([2 1 1]);
+subplot(2,2,4); hold on; plot(wls_snb,mel_stimt,'c'); plot(wls_snb,mel_stimm,'cx'); title('melanopsin stimulation'); plot(xlim, [0,0], 'k:'); pbaspect([2 1 1]);
+
+% calculate cone and melanopsin contrast
+L_cont = (L_stimt-L_stimm)./(L_stimm);
+M_cont = (M_stimt-M_stimm)./(M_stimm);
+S_cont = (S_stimt-S_stimm)./(S_stimm);
+mel_cont = (mel_stimt-mel_stimm)./(mel_stimm);
+
+% plot contrast as a function of test wavelength
+figure; hold on; pbaspect([2 1 1]);
 plot(wls_snb,mel_cont, 'c', 'LineWidth', 2)
 plot(wls_snb,L_cont, 'r--')
 plot(wls_snb,M_cont, 'g--')
 plot(wls_snb,S_cont, 'b--')
-legend({'melanopic contr', 'L cone contr', 'M cone contr', 'S cone contr'})
 plot(xlim, [0,0], 'k:')
+legend({'melanopic contr', 'L cone contr', 'M cone contr', 'S cone contr', ''})
+
+% plot S cone and melanopsin contrast separately
+figure; hold on
+subplot(2,1,1); hold on; plot(wls_snb,mel_cont, 'c', 'LineWidth', 2); title('Melanopic contrast'); plot(xlim, [0,0], 'k:')
+subplot(2,1,2); hold on; plot(wls_snb,mel_stimt, 'c'); plot(wls_snb,mel_stimm, 'cx'); 
+plot(xlim, [0,0], 'k:');  title('Melanopsin stimulation'); legend({'melanopsin test', 'melanopsin match',''})
+figure; hold on
+subplot(2,1,1); hold on; plot(wls_snb,S_cont, 'b', 'LineWidth', 2); title('S cone contrast'); plot(xlim, [0,0], 'k:')
+subplot(2,1,2); hold on; plot(wls_snb,S_stimt, 'b'); plot(wls_snb,S_stimm, 'bx'); 
+plot(xlim, [0,0], 'k:'); title('S cone stimulation'); legend({'S cone test', 'S cone match',''})
+
+% calculate cone and melanopsin difference in stimulation
+L_diff = (L_stimt-L_stimm);
+M_diff = (M_stimt-M_stimm);
+S_diff = (S_stimt-S_stimm);
+mel_diff = (mel_stimt-mel_stimm);
+
+% plot difference as a function of test wavelength
+figure; hold on; pbaspect([2 1 1]);
+plot(wls_snb,mel_diff, 'c', 'LineWidth', 2)
+plot(wls_snb,L_diff, 'r--')
+plot(wls_snb,M_diff, 'g--')
+plot(wls_snb,S_diff, 'b--')
+plot(xlim, [0,0], 'k:')
+legend({'melanopic contr', 'L cone contr', 'M cone contr', 'S cone contr', ''})
+
+%% 
+
+% CHNAGING PRIMARIES
+% -------------------
+wls = (380:1:780)';
+wls_test = (390:5:830)';
+
+% Construct match and test spectra
+r_match = 600;
+g_match = 500;
+b_match = 400;
+primaries = [r_match,g_match,b_match];
+
+n_test = length(wls_test);
+n_wls = length(wls);
+wls2 = repmat(wls,1,n_test);
+spdt = zeros(n_wls,n_test);
+spdm = zeros(n_wls,n_test);
+
+% constructing spectra assuming a gaussian shape of primaries and test
+FWHM = 10; % FWMH of gaussian function for spectra; based on interference filter properties
+sigma = FWHM/2.4; % stdev of gaussian
+gauss = @(height,position)height.*exp(-((wls-position).^2)/(2*sigma^2));
+
+% needs more work:
+opt_primaries = @(T_rgb) sum(((T_receptors(1:3,:)*((spdm(:,i)+gauss(T_rgb(i,1),r_match)+...
+    gauss(T_rgb(i,2),g_match)+gauss(T_rgb(i,3),b_match))...
+    - (spdt(:,i)+gauss(1,wls_test(i)))))./ ...
+    (T_receptors(1:3,:)*((spdm(:,i)+gauss(T_rgb(i,1),r_match)+...
+    gauss(T_rgb(i,2),g_match)+gauss(T_rgb(i,3),b_match))))).^2);
+
+
+startp = [1,1,1];
+minp = [-3,-3,-3];
+maxp = [3,3,3];
+options = optimset('TolCon', 1e-10000);
+
+for i=1:n_snb
+    T_rgb(i,:) = fmincon(opt_primaries,startp,[],[],[],[],minp,maxp,[], options);
+end
+
+
